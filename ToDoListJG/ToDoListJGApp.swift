b57218ca32c9e4635221b81ff0afdb9545686f19
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-22 16:07:42 -0600

    Initial Commit

---
+++ ToDoListJG/ToDoListJGApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListJGApp.swift
+//  ToDoListJG
+//
+//  Created by Mitch Andrade on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListJGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
