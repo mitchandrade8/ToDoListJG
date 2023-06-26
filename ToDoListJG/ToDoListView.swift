65778836f5288b83421622ffce8f5cf8f02083bb
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-26 09:42:04 -0600

    -- Starting new ToDoList project for reps on simple data handling and review

--- ToDoListJG/ToDoListView.swift
+++ ToDoListJG/ToDoListView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ToDoListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,6 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ToDoListView()
 }
+
