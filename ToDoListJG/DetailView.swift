6c8c387162dbe70916c1726dfe19bac551061aaa
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-26 11:29:49 -0600

    -- updating to current point

--- ToDoListJG/DetailView.swift
+++ ToDoListJG/DetailView.swift
@@ -12,20 +12,57 @@ struct DetailView: View {
     @State private var toDo = ""
     @State private var reminderIsOn = false
     @State private var dueDate = Date.now + (60*60*8)
+    @State private var notes = ""
+    @State private var isCompleted = false
     
     var passedValue: String
     
     var body: some View {
-        List {
-            TextField("Enter to do here", text: $toDo)
-                .font(.title)
-                .textFieldStyle(.roundedBorder)
-            
-            Toggle("Set Reminder", isOn: $reminderIsOn)
-                .padding(.top)
-            DatePicker("Date", selection: $dueDate)
+        NavigationStack {
+            List {
+                TextField("Enter to do here", text: $toDo)
+                    .font(.title)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.vertical)
+                    .listRowSeparator(.hidden)
+                
+                Toggle("Set Reminder", isOn: $reminderIsOn)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
+                
+                DatePicker("Date", selection: $dueDate)
+                    .listRowSeparator(.hidden)
+                    .padding(.bottom)
+                    .disabled(!reminderIsOn)
+                
+                Text("Notes:")
+                    .padding(.top)
+                TextField("Notes", text: $notes, axis: .vertical)
+                    .textFieldStyle(.roundedBorder)
+                    .listRowSeparator(.hidden)
+                
+                Toggle("Completed:", isOn: $isCompleted)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        // save
+                    }
+                }
+            }
+            .navigationBarBackButtonHidden()
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .listStyle(.plain)
+        
     }
 }
 
